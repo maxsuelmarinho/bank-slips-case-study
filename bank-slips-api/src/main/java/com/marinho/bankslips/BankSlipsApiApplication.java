7b802c9206b9e91d3b34b680eab8510645a4c2ce
Maxsuel Marihno <maxsuelmarinho@gmail.com>
2018-09-13 00:14:28 -0300

    Initial commit

---
+++ bank-slips-api/src/main/java/com/marinho/bankslips/BankSlipsApiApplication.java
@@ -0,0 +1,12 @@
+package com.marinho.bankslips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSlipsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSlipsApiApplication.class, args);
+	}
+}
