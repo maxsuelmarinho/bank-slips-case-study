233c339bd3d57e7dd06dd534fd936e3dcf746861
Maxsuel Marihno <maxsuelmarinho@gmail.com>
2018-09-13 02:42:17 -0300

    Add list bank slips endpoint

--- bank-slips-api/src/test/java/com/marinho/bankslips/controllers/BankSlipsControllerTests.java
+++ bank-slips-api/src/test/java/com/marinho/bankslips/controllers/BankSlipsControllerTests.java
@@ -5,14 +5,17 @@ import com.github.javafaker.Faker;
 import com.marinho.bankslips.dto.BankSlipRequest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.core.IsNull.notNullValue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -40,6 +43,7 @@ public class BankSlipsControllerTests {
         mvc.perform(
                 post("/")
                 .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
                 .content(mapper.writeValueAsString(request)))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.id", notNullValue()))
@@ -47,5 +51,19 @@ public class BankSlipsControllerTests {
                 .andExpect(jsonPath("$.total_in_cents", is(request.getTotalInCents())))
                 .andExpect(jsonPath("$.customer", is(request.getCustomer())))
                 .andExpect(jsonPath("$.status", is("PENDING")));
+    }
+
+    @Test
+    public void listBankSlips() throws Exception {
+        MvcResult result = mvc.perform(
+                get("/")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        final String expected = "[{id:\"asdasdasd\",due_date:\"2018-09-12\",total_in_cents:\"1000000000\",customer:\"Fake Company\",status:\"PENDING\"}," +
+                "{id:\"qweqweqwe\",due_date:\"2018-09-13\",total_in_cents:\"1000000001\",customer:\"Real Company\",status:\"PENDING\"}]";
+
+        JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false);
     }
 }
